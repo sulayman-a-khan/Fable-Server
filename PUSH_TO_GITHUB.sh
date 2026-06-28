b4d23a9ca006f7ffb412ca28495ccc55fe22b7d0
Sulayman <hellosulayman@gmail.com>
2026-06-28 14:25:33 +0600

    chore: add github push script

---
+++ PUSH_TO_GITHUB.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this script from within the server/ folder to push all commits to GitHub
+echo "Pushing server commits to GitHub..."
+git push origin main
+echo "Done! Check your GitHub repository."
